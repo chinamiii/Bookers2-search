2b5dcd544645fdf9bebda01a83a22c482b24a05b
chinamiii <chinami.2929@icloud.com>
2021-08-30 15:43:55 +0000

    [search]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @newbook = Book.new
     @books =@user.books
+    
   end
 
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
         has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
         has_many :followings, through: :relationships, source: :followed
-        
+
 
         attachment :profile_image
 
@@ -32,5 +32,18 @@ class User < ApplicationRecord
         def following?(user)
           followings.include?(user)
         end
-        
+
+    def self.search(search, word)
+      if search == "forward_match"
+        @user = User.where("name LIKE?", "#{word}%")
+      elsif search == "backward_match"
+        @user = User.where("name LIKE?", "%#{word}")
+      elsif search == "perfect_match"
+        @user = User.where("name LIKE?", "#{word}")
+      elsif search == "partial_match"
+        @user = User.where("name LIKE?", "%#{word}%")
+      else
+        @user = User.all
+      end
+    end
 end
